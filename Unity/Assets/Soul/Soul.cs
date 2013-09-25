edf6e5b04ca920a373e3bea8e9d4b454c17a43be
nnakata <nakata@team-lab.com>
2013-09-25 18:09:06 +0900

    brush up

--- Unity/Assets/Soul/Soul.cs
+++ Unity/Assets/Soul/Soul.cs
@@ -10,22 +10,30 @@ public class Soul : MonoBehaviour {
 	
 	private float _rDuration;
 	private float _startTime;
+	private State _state;
+	
+	enum State { Play = 0, BeingDestroyed }
 
 	// Use this for initialization
 	void Start () {
+		_state = State.Play;
 		_rDuration = 1f / duration;
 		_startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (_state != State.Play)
+			return;
+		
 		var t = (Time.time - _startTime) * _rDuration;
 		var pos = curve.Interpolate(t);
 		transform.position = pos;
 		if (t >= 1f) {
+			_state = State.BeingDestroyed;
 			if (OnArrived != null)
 				OnArrived(this);
-			Destroy(gameObject);
+			Destroy(gameObject, duration);
 		}
 	}
 }
